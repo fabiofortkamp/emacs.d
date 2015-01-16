68159c63a4f784d6e8568f0a1f53d08969dca98e
Fábio Fortkamp <fabio@fabiofortkamp.com>
2015-01-16 14:51:59 -0200

    Refreshed first commit; basic markdown mode, some UI customizations and AUCTEX

---
+++ emacs-custom.el
@@ -0,0 +1,38 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-view-program-list
+   (quote
+    (("Preview" "open %o -a \"Skim.app\"")
+     ("Sumatra" "SumatraPDF.exe %o"))))
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(comment-empty-lines t)
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(ns-alternate-modifier (quote none))
+ '(org-agenda-files
+   (quote
+    ("~/Documents/phd/disciplinas/em/multiphase-flow-class-notes/mf-class1.org")))
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("marmalade" . "http://marmalade-repo.org/packages/")
+     ("melpa" . "http://melpa.milkbox.net/packages/"))))
+ '(reftex-cite-format
+   (quote
+    ((13 . "\\cite{%l}")
+     (111 . "\\citeonline{%l}")
+     (112 . "[@%l]")
+     (116 . "@%l")
+     (105 . "%l"))))
+ '(reftex-label-alist (quote ((nil 101 nil "\\autoref{%s}" nil nil))))
+ '(reftex-ref-style-default-list (quote ("Default" "Hyperref"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,150 @@
+; specifify the custom file
+(setq custom-file "~/.emacs.d/emacs-custom.el")
+(load custom-file)
+
+; add init directory to load path
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+;disable tollbar
+(tool-bar-mode -1)
+
+; line numbers
+(global-linum-mode 1)
+
+; ido-mode
+(ido-mode 1)
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+
+; make newline automatically ident
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+(add-to-list 'load-path "~/.emacs.d/markdown-mode/")
+(require 'markdown-mode)
+(autoload 'markdown-mode "markdown-mode"
+       "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(markdown-enable-math)
+
+; diables dialog boxes on OS X, which are completely broke
+; http://superuser.com/questions/125569/how-to-fix-emacs-popup-dialogs-on-mac-os-x
+(defadvice yes-or-no-p (around prevent-dialog activate)
+  "Prevent yes-or-no-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
+(defadvice y-or-n-p (around prevent-dialog-yorn activate)
+  "Prevent y-or-n-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
+
+
+;; visual line mode breakes the lines at the window edges, but only visually -- the buffer displays as two lines, but there is only one "logical line"
+(global-visual-line-mode 1)
+
+;; commands suggested by the "Writing GNU Emacs Extensions" book
+
+(global-set-key "\M-?" 'help-command)
+(global-set-key "\C-h" 'delete-backward-char)
+
+					; OS-specific
+(if (eq system-type 'darwin)
+    (exec-path-from-shell-initialize)
+  (if (eq system-type 'windows-nt)
+      (progn
+	(set-face-font 'default "InputMono")
+	;; Make sure that the bash executable can be found
+	(setq explicit-shell-file-name "C:/cygwin64/bin/bash.exe")
+	(setq shell-file-name explicit-shell-file-name)
+	(add-to-list 'exec-path "C:/cygwin64/bin")
+	(set-variable 'TeX-view-program-selection
+	      '((output-pdf "Sumatra")))
+	)
+    )
+  )
+
+;---
+;;loading ELPA packages
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(load-theme 'solarized-light 1)
+
+(require 'org)
+
+;; Make RefTeX work with Org-Mode
+;; use 'C-c (' instead of 'C-c [' because the latter is already
+;; defined in orgmode to the add-to-agenda command.
+;; source: http://orgmode.org/worg/org-faq.html#using-reftex-in-org-mode
+
+(defun org-mode-reftex-setup ()
+  (load-library "reftex") 
+  (and (buffer-file-name)
+  (file-exists-p (buffer-file-name))
+  (reftex-parse-all))
+  (define-key org-mode-map (kbd "C-c (") 'reftex-citation))
+
+(add-hook 'org-mode-hook 'org-mode-reftex-setup)
+
+; enable markdown exporter
+; source: http://stackoverflow.com/questions/22988092/emacs-org-mode-export-markdown
+(eval-after-load "org"
+  '(require 'ox-md nil t))
+
+;; some nice AUCTEX configurations
+;; source: http://www.stefanom.org/setting-up-a-nice-auctex-environment-on-mac-os-x/
+;; AucTeX
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+(setq TeX-PDF-mode t)
+
+(eval-after-load "latex"
+'(define-key docTeX-mode-map (kbd "\C-c j") 'comment-indent-new-line))
+
+;; ;; Use Skim as viewer, enable source <-> PDF sync
+;; ;; make latexmk available via C-c C-c
+;; ;; Note: SyncTeX is setup via ~/.latexmkrc (see below)
+;; (add-hook 'LaTeX-mode-hook (lambda ()
+;;   (push
+;;     '("latexmk" "latexmk -pdf %s" TeX-run-TeX nil t
+;;       :help "Run latexmk on file")
+;;     TeX-command-list)))
+;; (add-hook 'TeX-mode-hook '(lambda () (setq TeX-command-default "latexmk")))
+ 
+;; ;; use Skim as default pdf viewer
+;; ;; Skim's displayline is used for forward search (from .tex to .pdf)
+;; ;; option -b highlights the current line; option -g opens Skim in the background  
+;; (setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
+;; (setq TeX-view-program-list
+;;      '(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
+
+
+; setup Reftex and enable in Markdown mode
+(require 'reftex)
+(add-hook 'markdown-mode-hook 'turn-on-reftex)
+
+; enable reftex in LaTeX mode
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)   ; with AUCTeX LaTeX mode
+(add-hook 'latex-mode-hook 'turn-on-reftex)   ; with Emacs latex mode
+
+; setup pandoc mode
+(require 'pandoc-mode)
+
+(add-hook 'markdown-mode-hook 'turn-on-pandoc)
+
+;(exec-path-from-shell-initialize)
+
+(require 'multi-term)
+(global-set-key (kbd "<f5>") 'multi-term)
+
+(require 'bookmark+)
+
+(put 'upcase-region 'disabled nil)
+
+
+

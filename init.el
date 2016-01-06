ec10d819adce73dd5810fc9b16375d5ccc0d53a7
Fabio Fortkamp <fabio@fabiofortkamp.com>
2016-01-06 17:05:46 -0200

    Removed key settings from yasnippet; the default TAB key is now used

--- init.el
+++ init.el
@@ -65,9 +65,6 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 
-(define-key yas-minor-mode-map (kbd "<tab>") nil)
-(define-key yas-minor-mode-map (kbd "TAB") nil)
-(define-key yas-minor-mode-map (kbd "C-<tab>") 'yas-expand)
 
 
 ;; NOTES

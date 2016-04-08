4a0b1c9c820f66d11bf9e470c86765340d16ce22
Fabio Fortkamp <fabio@fabiofortkamp.com>
2016-04-08 17:04:30 -0300

    Included helm-mode

--- init.el
+++ init.el
@@ -49,6 +49,7 @@
     polymode
     ess
     ein
+    helm
     ))
 
 (mapc #'(lambda (package)
@@ -75,9 +76,10 @@
 ;; MINOR PACKAGES
 
 ; setup IDO mode
-(ido-mode 1)
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
+; [temporarily disabled to use helm]
+;; (ido-mode 1)
+;; (setq ido-enable-flex-matching t)
+;; (setq ido-everywhere t)
 
 ; enable the expand-region package
 (add-to-list 'load-path (expand-file-name "expand-region.el" dotfiles-dir))
@@ -450,4 +452,38 @@ bibliography: [non-fiction.bib, Thermo-Foam-Ref.bib]
 
 (add-to-list 'auto-mode-alist '("\\.Pnw" . poly-pweave-mode))
 
+;; helm
+
+; from the official wiki: https://tuhdo.github.io/helm-intro.html
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-c C-m") 'helm-M-x)
+
+
+(setq helm-mode-fuzzy-match t)
+(setq helm-completion-in-region-fuzzy-match t)
+(setq helm-M-x-fuzzy-match t)
+
+(helm-mode 1)
 ;;; init.el ends here

4e4c95fd269e7e49194246a86bb4a6edccef6508
Fabio Fortkamp <fabio@fabiofortkamp.com>
2017-09-11 11:54:45 +0200

    Changed default markdown mode to the more useful gfm-mode

--- init.el
+++ init.el
@@ -138,9 +138,9 @@
 (require 'markdown-mode)
 (autoload 'markdown-mode "markdown-mode"
        "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.txt\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
+
 
 (markdown-toggle-math)
 

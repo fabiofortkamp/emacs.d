c5362b8fc0576c068c2eee917eb8c2f3131b11a7
Fabio Fortkamp <fabio@fabiofortkamp.com>
2018-06-14 13:12:39 -0300

    Added prototype function to export zettel to latex

--- init.el
+++ init.el
@@ -277,7 +277,28 @@ classoption: [brazil,english]
         )
       )
 
+(defun zettel-to-latex ()
+  "Export text from buffer or selected region as LaTeX to clipboard"
+  (interactive)
+  (let (
+	(start (if (region-active-p) (region-beginning) (point-min)))
+	(end (if (region-active-p) (region-end) (point-max)))
+	(created-buffer-name "*Zettel-to-LaTeX output*")
+
+	)
+    (progn
+      (shell-command-on-region start end "pandoc --from=markdown+raw_tex --to=latex --top-level-division=chapter --natbib" created-buffer-name)
+  ; save output from above to string and save it to clipboard
+      (with-current-buffer created-buffer-name
+	(kill-ring-save (point-min) (point-max))
+	)
+      (kill-buffer created-buffer-name)
+      )
+  )
+  )
+
 (define-key markdown-mode-map (kbd "C-c k") 'thermo-emacs-copy-zettel-id)
+(define-key markdown-mode-map (kbd "C-c p") 'zettel-to-latex)
 
 
 ;; LATEX
